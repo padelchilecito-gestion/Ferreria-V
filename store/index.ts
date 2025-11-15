7e2d556d517420c94bffd0f4526b52e0033f1b77
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-15 10:49:34 -0300

    Update index.ts

--- store/index.ts
+++ store/index.ts
@@ -1,16 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from './productsSlice';
-// Importaremos más reducers aquí (customersReducer, cartReducer, etc.)
+import cartReducer from './cartSlice'; // 1. Importar el nuevo reducer
 
 export const store = configureStore({
   reducer: {
-    // Aquí definimos cómo se verá tu estado global
-    // state.products será manejado por productsReducer
     products: productsReducer,
-    // ... aquí irán los demás
+    cart: cartReducer, // 2. Añadir el reducer del carrito al store
   },
 });
 
-// Tipos importantes para usar con TypeScript en tus componentes
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
