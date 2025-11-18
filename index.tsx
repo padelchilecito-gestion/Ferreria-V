21c589d1a57b86af9ba17081f45b73e3bdb705d4
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-18 11:08:59 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,7 +4,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
-import './index.css'; // <--- ¡ESTA LÍNEA ES LA CLAVE! ASEGÚRATE DE QUE EXISTA
+
+// --- ESTA LÍNEA ES LA MÁS IMPORTANTE AHORA ---
+import './index.css'; 
+// ---------------------------------------------
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
