55420dd7f21f51f895e8d3fe0dda24c26e157ea6
padelclilecito-gestion <padelchilecito@gmail.com>
2025-11-18 11:03:59 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store, persistor } from './store'; // 1. Importar el persistor
-import { PersistGate } from 'redux-persist/integration/react'; // 2. Importar PersistGate
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import './index.css'; // <--- ¡ESTA LÍNEA ES LA CLAVE! ASEGÚRATE DE QUE EXISTA
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -14,7 +15,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* 3. Envolver la App con PersistGate */}
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
